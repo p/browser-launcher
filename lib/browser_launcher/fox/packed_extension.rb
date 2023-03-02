b389e4663de096cf5d0a700afcfd438bb4850111
Oleg Pudeyev <code@olegp.name>
2023-03-02 03:41:14 -0500

    refactor fox code

---
+++ lib/browser_launcher/fox/packed_extension.rb
@@ -0,0 +1,50 @@
+require 'zip'
+require 'active_support'
+require 'active_support/core_ext/hash/conversions'
+
+class PackedExtension
+  def initialize(path)
+    @path = path
+  end
+
+  attr_reader :path
+
+  def ext_id
+    desc.fetch('id')
+  end
+
+  %w(bootstrap unpack version name description creator).each do |key|
+    define_method(key) do
+      desc.fetch(key)
+    end
+  end
+
+  def target_application
+    desc.fetch('targetApplication')
+  end
+
+  def metadata
+    @metadata ||= begin
+      install_xml = nil
+      Zip::File.open(path) do |zip|
+        zip.each do |entry|
+          if entry.name == 'install.rdf'
+            install_xml = entry.get_input_stream.read
+          end
+        end
+      end
+
+      if install_xml.nil?
+        raise "No install.rdf in #{path} - not a fox extension?"
+      end
+
+      Hash.from_xml(install_xml)
+    end
+  end
+
+  private
+
+  def desc
+    @desc ||= metadata.fetch('RDF').fetch('Description')
+  end
+end
