9fedf0d49bd9a35095a592ba646969adfeacefec
Oleg Pudeyev <code@olegp.name>
2023-03-22 14:41:37 -0400

    moving classes under namespace wip

--- lib/browser_launcher/fox/extension_metadata.rb
+++ lib/browser_launcher/fox/extension_metadata.rb
@@ -2,53 +2,57 @@ require 'active_support'
 require 'active_support/core_ext/hash/indifferent_access'
 require 'json'
 
-class ExtensionsMetadata
-  def initialize(path)
-    @path = path
+module BrowserLauncher
+  module Fox
+    class ExtensionsMetadata
+      def initialize(path)
+        @path = path
 
-    load!
-  end
-
-  attr_reader :path
-
-  def load!
-    data = if File.exist?(path)
-      JSON.load(File.read(path))
-    else
-      {
-        schemaVersion: 24,
-        addons: addons,
-      }
-    end
-    @data = ActiveSupport::HashWithIndifferentAccess.new(data)
-  end
-
-  attr_reader :data
-
-  def addon(name)
-    data.fetch(:addons).detect do |addon|
-      # Not all addons have names
-      addon[:name] == name
-    end
-  end
-
-  def set_addon(name, info)
-    found = false
-    data.fetch(:addons).each do |addon|
-      if addon[:name] == name
-        found = true
-        addon.update(info)
-        break
+        load!
       end
-    end
-    unless found
-      data[:addons] << ActiveSupport::HashWithIndifferentAccess.new(info).update('name' => name)
-    end
-  end
 
-  def dump!
-    File.open(path, 'w') do |f|
-      f << data
+      attr_reader :path
+
+      def load!
+        data = if File.exist?(path)
+          JSON.load(File.read(path))
+        else
+          {
+            schemaVersion: 24,
+            addons: addons,
+          }
+        end
+        @data = ActiveSupport::HashWithIndifferentAccess.new(data)
+      end
+
+      attr_reader :data
+
+      def addon(name)
+        data.fetch(:addons).detect do |addon|
+          # Not all addons have names
+          addon[:name] == name
+        end
+      end
+
+      def set_addon(name, info)
+        found = false
+        data.fetch(:addons).each do |addon|
+          if addon[:name] == name
+            found = true
+            addon.update(info)
+            break
+          end
+        end
+        unless found
+          data[:addons] << ActiveSupport::HashWithIndifferentAccess.new(info).update('name' => name)
+        end
+      end
+
+      def dump!
+        File.open(path, 'w') do |f|
+          f << data
+        end
+      end
     end
   end
 end
--- lib/browser_launcher/fox/packed_extension.rb
+++ lib/browser_launcher/fox/packed_extension.rb
@@ -2,49 +2,53 @@ require 'zip'
 require 'active_support'
 require 'active_support/core_ext/hash/conversions'
 
-class PackedExtension
-  def initialize(path)
-    @path = path
-  end
+module BrowserLauncher
+  module Fox
+    class PackedExtension
+      def initialize(path)
+        @path = path
+      end
 
-  attr_reader :path
+      attr_reader :path
 
-  def ext_id
-    desc.fetch('id')
-  end
+      def ext_id
+        desc.fetch('id')
+      end
 
-  %w(bootstrap unpack version name description creator).each do |key|
-    define_method(key) do
-      desc.fetch(key)
-    end
-  end
-
-  def target_application
-    desc.fetch('targetApplication')
-  end
-
-  def metadata
-    @metadata ||= begin
-      install_xml = nil
-      Zip::File.open(path) do |zip|
-        zip.each do |entry|
-          if entry.name == 'install.rdf'
-            install_xml = entry.get_input_stream.read
-          end
+      %w(bootstrap unpack version name description creator).each do |key|
+        define_method(key) do
+          desc.fetch(key)
         end
       end
 
-      if install_xml.nil?
-        raise "No install.rdf in #{path} - not a fox extension?"
+      def target_application
+        desc.fetch('targetApplication')
       end
 
-      Hash.from_xml(install_xml)
+      def metadata
+        @metadata ||= begin
+          install_xml = nil
+          Zip::File.open(path) do |zip|
+            zip.each do |entry|
+              if entry.name == 'install.rdf'
+                install_xml = entry.get_input_stream.read
+              end
+            end
+          end
+
+          if install_xml.nil?
+            raise "No install.rdf in #{path} - not a fox extension?"
+          end
+
+          Hash.from_xml(install_xml)
+        end
+      end
+
+      private
+
+      def desc
+        @desc ||= metadata.fetch('RDF').fetch('Description')
+      end
     end
-  end
-
-  private
-
-  def desc
-    @desc ||= metadata.fetch('RDF').fetch('Description')
   end
 end
