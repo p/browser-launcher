6eac3d2be37301e0dc4e3abae79a11758d4fb301
Oleg Pudeyev <code@olegp.name>
2023-03-27 17:48:05 -0400

    handle chrome extensions in fox

--- lib/browser_launcher/fox/packed_extension.rb
+++ lib/browser_launcher/fox/packed_extension.rb
@@ -9,47 +9,62 @@ module BrowserLauncher
     class PackedExtension
       def initialize(path)
         @path = path
+
+        load!
       end
 
       attr_reader :path
 
-      def ext_id
-        desc.fetch('id')
-      end
+      attr_reader :ext_id, :bootstrap, :unpack, :version,
+        :name, :description, :creator, :target_application
 
-      %w(bootstrap unpack version name description creator).each do |key|
-        define_method(key) do
-          desc.fetch(key)
-        end
-      end
+      def load!
+        install_xml = nil
+        manifest_json = nil
 
-      def target_application
-        desc.fetch('targetApplication')
-      end
-
-      def metadata
-        @metadata ||= begin
-          install_xml = nil
-          Zip::File.open(path) do |zip|
-            zip.each do |entry|
-              if entry.name == 'install.rdf'
-                install_xml = entry.get_input_stream.read
-              end
+        Zip::File.open(path) do |zip|
+          zip.each do |entry|
+            if entry.name == 'install.rdf'
+              install_xml = entry.get_input_stream.read
+            end
+            if entry.name == 'manifest.json'
+              manifest_json = entry.get_input_stream.read
             end
           end
+        end
 
-          if install_xml.nil?
-            raise "No install.rdf in #{path} - not a fox extension?"
-          end
+        if install_xml.nil? && manifest_json.nil?
+          raise "No install.rdf or manifest.json in #{path} - not a fox extension?"
+        end
 
-          Hash.from_xml(install_xml)
+        if install_xml && manifest_json
+          raise "Both install.rdf and manifest.json are present in #{path} - don't know which to choose"
+        end
+
+        if install_xml
+          load_rdf(install_xml)
+        else
+          load_chrome(manifest_json)
         end
       end
 
-      private
+      def load_rdf(contents)
+        metadata = Hash.from_xml(contents)
+        desc = metadata.fetch('RDF').fetch('Description')
+        %w(bootstrap unpack version name description creator).each do |key|
+          instance_variable_set("@#{key}", desc.fetch(key))
+        end
+        @ext_id = desc.fetch('id')
+        @target_application = desc.fetch('targetApplication')
+      end
 
-      def desc
-        @desc ||= metadata.fetch('RDF').fetch('Description')
+      def load_chrome(contents)
+        metadata = JSON.parse(contents)
+        @ext_id = metadata.fetch('applications').fetch('gecko').fetch('id')
+        @version = metadata.fetch('version')
+        @name = metadata.fetch('name')
+        @description = metadata.fetch('description')
+        @creator = metadata.fetch('author')
       end
     end
   end
